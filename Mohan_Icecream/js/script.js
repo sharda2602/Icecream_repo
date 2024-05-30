fe6381df0621cff9600d31ea12af5b8351e6fffa
Sharda Pardeshi <pardeshisharda26@gmail.com>
2024-05-30 20:51:04 +0530

    ice cream website

---
+++ Mohan_Icecream/js/script.js
@@ -0,0 +1,31 @@
+let currentSlide = 0;
+
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.carousel-slide').style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+setInterval(nextSlide, 4000); // Change slide every 3 seconds
+
+showSlide(currentSlide);
+
+
+
